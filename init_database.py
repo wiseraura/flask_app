f76652062818b87bec4dadfbc040d9f8bc4de766
wiseraura <wiseraura@gmail.com>
2023-04-24 17:07:47 +0700

    Add more character to author columm

--- init_database.py
+++ init_database.py
@@ -21,7 +21,7 @@ cur.execute('CREATE TABLE accounts (id serial PRIMARY KEY,'
 
 cur.execute('CREATE TABLE books (id serial PRIMARY KEY,'
             'title varchar (150) NOT NULL,'
-            'author varchar (50) NOT NULL,'
+            'author varchar (100) NOT NULL,'
             'pages_num integer NOT NULL,'
             'review text,'
             'date_added date DEFAULT CURRENT_TIMESTAMP);'
