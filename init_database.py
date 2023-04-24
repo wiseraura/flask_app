d6e54e11af86875515285d074ebdc9d486b1aab6
wiseraura <wiseraura@gmail.com>
2023-04-24 16:08:36 +0700

    Add accounts table

--- init_database.py
+++ init_database.py
@@ -2,23 +2,30 @@ import psycopg2
 
 # Open a connection to the database
 conn = psycopg2.connect(
-        host="localhost",
-        database="flask_database",
-        user="postgres",
-        password="123456789Aa@")
+    host="localhost",
+    database="flask_database",
+    user="postgres",
+    password="123456789Aa@")
 
 # Open a cursor to perform database operations
 cur = conn.cursor()
 
 # Execute a command: this creates a new table
 cur.execute('DROP TABLE IF EXISTS books;')
+cur.execute('DROP TABLE IF EXISTS accounts;')
+
+cur.execute('CREATE TABLE account (id serial PRIMARY KEY,'
+            'username varchar (15) NOT NULL,'
+            'password varchar(20) NOT NULL);'
+            )
+
 cur.execute('CREATE TABLE books (id serial PRIMARY KEY,'
-                                 'title varchar (150) NOT NULL,'
-                                 'author varchar (50) NOT NULL,'
-                                 'pages_num integer NOT NULL,'
-                                 'review text,'
-                                 'date_added date DEFAULT CURRENT_TIMESTAMP);'
-                                 )
+            'title varchar (150) NOT NULL,'
+            'author varchar (50) NOT NULL,'
+            'pages_num integer NOT NULL,'
+            'review text,'
+            'date_added date DEFAULT CURRENT_TIMESTAMP);'
+            )
 
 # Insert data into the table
 cur.execute('INSERT INTO books (title, author, pages_num, review)'
@@ -28,7 +35,6 @@ cur.execute('INSERT INTO books (title, author, pages_num, review)'
              489,
              'A great classic!')
             )
-
 
 cur.execute('INSERT INTO books (title, author, pages_num, review)'
             'VALUES (%s, %s, %s, %s)',
